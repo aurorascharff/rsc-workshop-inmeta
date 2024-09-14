ba0cf3267cdc44ab3529ce75163e921aa3c4b83e
Aurora Scharff <aurora.sofie@gmail.com>
2024-09-14 14:27:42 +0200

    Add suspense page

--- app/(intro)/suspense/slow/page.tsx
+++ app/(intro)/suspense/slow/page.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { slow } from '@/utils/slow';
 
-export default async function SlowPage() {
+async function SlowComponent() {
   await slow();
 
-  return <div>SlowPage</div>;
+  return <div>SlowComponent</div>;
+}
+
+export default async function SlowPage() {
+  // Mens vi venter på denne, rendres loading.tsx som fallback
+  await slow();
+
+  return (
+    <div>
+      SlowPage
+      <Suspense fallback={<div>Loading slow component...</div>}>
+        <SlowComponent />
+      </Suspense>
+    </div>
+  );
 }
