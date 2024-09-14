cd436d5c93b539e95fab2624dce550deeb8ab8f5
Aurora Scharff <aurora.sofie@gmail.com>
2024-09-14 13:46:53 +0200

    Add transition to contactButton

--- components/ContactButton.tsx
+++ components/ContactButton.tsx
@@ -1,21 +1,30 @@
 'use client';
 
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import React from 'react';
+import { usePathname, useRouter } from 'next/navigation';
+import React, { useTransition } from 'react';
 import { cn } from '@/utils/cn';
 import type { Contact } from '@prisma/client';
 
 export default function ContactButton({ contact }: { contact: Contact }) {
   const pathname = usePathname();
   const isActive = pathname.includes(`/contacts/${contact.id}`);
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
 
   return (
     <Link
       className={cn(
+        isPending ? 'pending' : '',
         isActive ? 'bg-primary text-white' : 'hover:bg-gray',
         'flex w-full items-center justify-between gap-4 overflow-hidden whitespace-pre rounded-lg p-2 hover:no-underline',
       )}
+      onClick={e => {
+        e.preventDefault();
+        startTransition(() => {
+          router.push(`/contacts/${contact.id}`);
+        });
+      }}
       href={`/contacts/${contact.id}`}
     >
       {contact.first || contact.last ? (
