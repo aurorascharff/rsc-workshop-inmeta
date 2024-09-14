60fad06f60f5eded00df692761cd15f1a3a373df
Aurora Scharff <aurora.sofie@gmail.com>
2024-09-14 17:17:16 +0200

    Update CSS

--- app/contacts/[contactId]/edit/loading.tsx
+++ app/contacts/[contactId]/edit/loading.tsx
@@ -4,7 +4,7 @@ import Skeleton from '@/components/ui/Skeleton';
 export default function Loading() {
   return (
     <div className="flex max-w-[40rem] flex-col gap-4 @container">
-      <div className="grip-rows-6 grid grid-cols-1 gap-4 @sm:grid-cols-[1fr_4fr]">
+      <div className="grip-rows-6 grid gap-4 @sm:grid-cols-[1fr_4fr]">
         <div className="hidden flex-col gap-[72px] @sm:flex @sm:gap-8">
           <span className="flex">Name</span>
           <span>Position</span>
