1aaf34fc66374c39585ab79e55d37c49e756f1dd
Aurora Scharff <aurora.sofie@gmail.com>
2024-09-14 11:57:55 +0200

    Implement search feature

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,8 +1,13 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
 import React from 'react';
 import { SearchIcon, SpinnerIcon } from './ui/icons';
 
 export default function Search() {
   const searching = false;
+  const searchParams = useSearchParams();
+  const q = searchParams.get('q') || '';
 
   return (
     <form role="search">
@@ -10,6 +15,7 @@ export default function Search() {
         className="w-full pl-8 outline-offset-1"
         aria-label="Search contacts"
         name="q"
+        defaultValue={q}
         placeholder="Search"
         type="search"
       />
