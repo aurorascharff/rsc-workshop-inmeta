c38b8c357ecccdb83501c0bd68b0dec2c4a071e2
Aurora Scharff <aurora.sofie@gmail.com>
2024-09-14 14:49:06 +0200

    Use server action

--- app/(intro)/client-server/_components/ClientComponent.tsx
+++ app/(intro)/client-server/_components/ClientComponent.tsx
@@ -5,17 +5,20 @@ import React from 'react';
 type Props = {
   children: React.ReactNode;
   content: React.ReactNode;
+  mutateData: () => Promise<string>;
 };
 
-export default function ClientComponent({ children, content }: Props) {
+export default function ClientComponent({ children, content, mutateData }: Props) {
   // Uten "use client" kan vi ikke bruke browser-ting som state, effetcs, onclick, wind.location osv
   return (
     <div className="border-2 border-red-500 p-4">
       {children}
       {content}
       <button
-        onClick={() => {
-          return alert('Hello');
+        onClick={async () => {
+          // Uten "use server" i mutateData kan vi ikke bruke denne
+          const data = await mutateData();
+          return alert(data);
         }}
       >
         Click me
--- app/(intro)/client-server/page.tsx
+++ app/(intro)/client-server/page.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { prisma } from '@/db';
 import ClientComponent from './_components/ClientComponent';
 import ServerComponent from './_components/ServerComponent';
 
 export default function ClientServerPage() {
+  // Denne funksjonen får et skjult API-endepunkt generert pga "use server"
+  async function mutateData(): Promise<string> {
+    'use server';
+
+    const data = await prisma.contact.findMany();
+    return data[0].first ?? '';
+  }
+
   return (
     <div>
-      <ClientComponent content={<ServerComponent />}>
+      <ClientComponent mutateData={mutateData} content={<ServerComponent />}>
         <ServerComponent />
       </ClientComponent>
     </div>
