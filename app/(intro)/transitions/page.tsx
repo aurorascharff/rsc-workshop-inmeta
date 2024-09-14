6517056e1434694670cdcf33c1bbddecfdda067a
Aurora Scharff <aurora.sofie@gmail.com>
2024-09-14 13:23:24 +0200

    Add tabs to transitions page

--- app/(intro)/transitions/page.tsx
+++ app/(intro)/transitions/page.tsx
@@ -1,5 +1,40 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
+import Button from '@/components/ui/Button';
 
 export default function TransitionsPage() {
-  return <div>TransitionsPage</div>;
+  const [tab, setTab] = useState(1);
+
+  return (
+    <>
+      <h1>Transitions</h1>
+      <div className="flex gap-4">
+        <Button
+          onClick={() => {
+            return setTab(1);
+          }}
+        >
+          Tab 1 {tab === 1 && '👈'}
+        </Button>
+        <Button
+          onClick={() => {
+            return setTab(2);
+          }}
+        >
+          Tab 2 {tab === 2 && '👈'}
+        </Button>
+        <Button
+          onClick={() => {
+            return setTab(3);
+          }}
+        >
+          Tab 3 {tab === 3 && '👈'}
+        </Button>
+      </div>
+      {tab === 1 && <div>Tab 1</div>}
+      {tab === 2 && <div>Tab 2</div>}
+      {tab === 3 && <div>Tab 3</div>}
+    </>
+  );
 }
