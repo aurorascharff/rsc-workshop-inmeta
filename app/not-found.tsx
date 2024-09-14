998d6942b9142e2ac4d6097c322cb3ce3f4fb80c
Aurora Scharff <aurora.sofie@gmail.com>
2024-09-14 11:41:37 +0200

    Add global notfound

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col gap-4">
+      <h1>404 Not Found</h1>
+      <p>Sorry, the page youre looking for does not exist.</p>
+    </div>
+  );
+}
