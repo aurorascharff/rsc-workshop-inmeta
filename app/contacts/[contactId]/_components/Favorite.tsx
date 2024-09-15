7239023461beae6735107a05f5162dcccb12ed0d
Aurora Scharff <aurora.sofie@gmail.com>
2024-09-15 11:38:01 +0200

    Add optimistic update to favorite

--- app/contacts/[contactId]/_components/Favorite.tsx
+++ app/contacts/[contactId]/_components/Favorite.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useOptimistic, useTransition } from 'react';
 import { favoriteContact } from '@/data/actions/favoriteContact';
 import { cn } from '@/utils/cn';
 import type { Contact } from '@prisma/client';
@@ -8,18 +8,29 @@ import type { Contact } from '@prisma/client';
 export default function FavoritePessimistic({ contact }: { contact: Contact }) {
   const favorite = contact.favorite;
   const favoriteContactById = favoriteContact.bind(null, contact.id, favorite);
+  const [optimisticFavorite, addOptimisticFavorite] = useOptimistic(favorite);
+  const [, startTransition] = useTransition();
 
   return (
-    <form action={favoriteContactById}>
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        startTransition(async () => {
+          addOptimisticFavorite(!optimisticFavorite);
+          await favoriteContactById();
+        });
+      }}
+      action={favoriteContactById}
+    >
       <button
         type="submit"
         className={cn(
-          favorite ? 'text-yellow-500' : 'text-gray-dark',
+          optimisticFavorite ? 'text-yellow-500' : 'text-gray-dark',
           'flex text-2xl font-normal shadow-none hover:text-yellow-400 hover:shadow-none',
         )}
-        aria-label={favorite ? 'Remove from favorites' : 'Add to favorites'}
+        aria-label={optimisticFavorite ? 'Remove from favorites' : 'Add to favorites'}
       >
-        {favorite ? '★' : '☆'}
+        {optimisticFavorite ? '★' : '☆'}
       </button>
     </form>
   );
