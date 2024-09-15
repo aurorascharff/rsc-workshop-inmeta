581cc9a1a378a66cbe463f66884d4c5baf46381d
Aurora Scharff <aurora.sofie@gmail.com>
2024-09-15 10:00:01 +0200

    Use form in favorite

--- app/contacts/[contactId]/_components/Favorite.tsx
+++ app/contacts/[contactId]/_components/Favorite.tsx
@@ -1,29 +1,26 @@
 'use client';
 
-import React, { useTransition } from 'react';
+import React from 'react';
 import { favoriteContact } from '@/data/actions/favoriteContact';
 import { cn } from '@/utils/cn';
 import type { Contact } from '@prisma/client';
 
 export default function FavoritePessimistic({ contact }: { contact: Contact }) {
   const favorite = contact.favorite;
-  const [isPending, startTransition] = useTransition();
+  const favoriteContactById = favoriteContact.bind(null, contact.id, favorite);
 
   return (
-    <button
-      onClick={() => {
-        startTransition(async () => {
-          await favoriteContact(contact.id, favorite);
-        });
-      }}
-      type="submit"
-      className={cn(
-        favorite || isPending ? 'text-yellow-500' : 'text-gray-dark',
-        'flex text-2xl font-normal shadow-none hover:text-yellow-400 hover:shadow-none',
-      )}
-      aria-label={favorite ? 'Remove from favorites' : 'Add to favorites'}
-    >
-      {favorite && !isPending ? '★' : '☆'}
-    </button>
+    <form action={favoriteContactById}>
+      <button
+        type="submit"
+        className={cn(
+          favorite ? 'text-yellow-500' : 'text-gray-dark',
+          'flex text-2xl font-normal shadow-none hover:text-yellow-400 hover:shadow-none',
+        )}
+        aria-label={favorite ? 'Remove from favorites' : 'Add to favorites'}
+      >
+        {favorite ? '★' : '☆'}
+      </button>
+    </form>
   );
 }
--- data/actions/favoriteContact.tsx
+++ data/actions/favoriteContact.tsx
@@ -1,6 +1,6 @@
 'use server';
 
-import { revalidatePath, revalidateTag } from 'next/cache';
+import { revalidatePath } from 'next/cache';
 import { prisma } from '@/db';
 import { slow } from '@/utils/slow';
 
