100c7ec89ccb373270cc2e26c5b5dc7bbf75eebc
Aurora Scharff <aurora.sofie@gmail.com>
2024-09-15 13:28:45 +0200

    Update

--- app/contacts/[contactId]/page.tsx
+++ app/contacts/[contactId]/page.tsx
@@ -4,12 +4,28 @@ import { getContact } from '@/data/services/getContact';
 import GithubLogo from '@/public/github-mark.svg';
 import DeleteContactButton from './_components/DeleteContactButton';
 import Favorite from './_components/Favorite';
+import type { Metadata } from 'next';
 
 type PageProps = {
   params: {
     contactId: string;
   };
 };
+
+// In local development, the `generateMetadata` will not be streamed and will block the page until it resolves, hindering the suspense boundary from showing.
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const contact = await getContact(params.contactId);
+
+  return contact && contact.first && contact.last
+    ? {
+        description: `Contact details for ${contact.first} ${contact.last}`,
+        title: `${contact.first} ${contact.last}`,
+      }
+    : {
+        description: 'Contact details for an unnamed contact',
+        title: 'Unnamed Contact',
+      };
+}
 
 export default async function ContactPage({ params }: PageProps) {
   const contact = await getContact(params.contactId);
