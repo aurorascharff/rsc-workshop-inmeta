e2ff7ee69621982d80c9ed48a75ddf13e08ec0fc
Aurora Scharff <aurora.sofie@gmail.com>
2024-09-15 13:32:26 +0200

    Update

--- data/services/getContact.ts
+++ data/services/getContact.ts
@@ -1,10 +1,13 @@
 import 'server-only';
 
 import { notFound } from 'next/navigation';
+import { cache } from 'react';
 import { prisma } from '@/db';
 import { slow } from '@/utils/slow';
 
-export async function getContact(contactId: string) {
+// without cache, this function would be called twice for [contactId]/page.tsx render
+export const getContact = cache(async (contactId: string) => {
+  console.log('getContact', contactId);
   await slow();
 
   const contact = await prisma.contact.findUnique({
@@ -17,4 +20,4 @@ export async function getContact(contactId: string) {
   }
 
   return contact;
-}
+});
